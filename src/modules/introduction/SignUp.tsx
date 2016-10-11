18e046e186a6b065cc27293e2427c43b175ffca0
bx <yangyuchen@witenet.com>
2016-10-11 22:54:07 +0800

    footer button

--- src/modules/introduction/SignUp.tsx
+++ src/modules/introduction/SignUp.tsx
@@ -59,17 +59,21 @@ export default class SignUp extends React.Component<any, any> {
 
 		return (
 			<div className="signup">
-				<div className="top-panel">
-					<img src={data.introPic} alt=""/>
+				<div style={{paddingBottom: 70}}>
+					<div className="top-panel">
+						<img src={data.introPic} alt=""/>
+					</div>
+					<div className="introduction">
+						{ data.voice ? <audio src={data.voice} controls="controls"/> : null}
+						{ /**data.voice ? <Audio url={data.voice}/> : null**/}
+						<div className="text" dangerouslySetInnerHTML={{__html: data.intro}}></div>
+					</div>
 				</div>
-				<div className="introduction">
-					{ data.voice ? <audio src={data.voice} controls="controls"/> : null}
-					{ /**data.voice ? <Audio url={data.voice}/> : null**/}
-					<div className="text" dangerouslySetInnerHTML={{__html: data.intro}}></div>
+				<section className="footer-btn">
 					<div className="btn-container">
 						<Button onClick={this.signup.bind(this)} plain>我要报名</Button>
 					</div>
-				</div>
+				</section>
 			</div>
 		)
 	}
