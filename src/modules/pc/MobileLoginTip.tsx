4ec74fe7eec540587602362cd90272447972834a
nethunder <17089113140@163.com>
2017-01-05 17:22:32 +0800

    添加反馈页面

---
+++ src/modules/pc/MobileLoginTip.tsx
@@ -0,0 +1,32 @@
+import * as React from "react"
+import "./MobileLoginTip.less"
+import {connect} from "react-redux"
+import {set, startLoad, endLoad} from "redux/actions"
+import {Msg} from "react-weui"
+import Icon from "../../components/Icon"
+import * as _ from "lodash"
+
+@connect(state => state)
+export default class Main extends React.Component<any, any> {
+
+  static contextTypes = {
+    router: React.PropTypes.object.isRequired
+  }
+
+  render() {
+    const {location} = this.props;
+    const err = _.get(location, "query.err");
+    return (
+      <div className="pcTipContainer">
+        <div className="success-img">
+          <Icon type={err?"false":"true"} size="150"/>
+        </div>
+        <div className="success-title">{err?err:"扫码登录成功!"}</div>
+        {/**<div className="success-msg">
+         <p>留意本周六的消息,圈圈姐帮你点评作业!</p>
+         <p>记得去群里讨论!</p>
+         </div>**/}
+      </div>
+    )
+  }
+}
