66c966f3d6409c9ddefbe17f637c456013b9785d
nethunder <17089113140@163.com>
2017-02-04 19:00:16 +0800

    fix

--- src/modules/survey/Survey.tsx
+++ src/modules/survey/Survey.tsx
@@ -12,7 +12,7 @@ export default class Reject extends React.Component<any,any> {
   }
 
   clickShareConfigBtn(){
-    configShareFriend("你的打分会左右RISE的方向","感谢您对我们的支持",`${window.location.host}/survey/wjx?activity=11918087`,"http://www.iquanwai.com/images/logo.png");
+    configShareFriend("投资5分钟，让RISE和你的思维一起升级","考验你对圈外真爱的时候到了",`${window.location.host}/survey/wjx?activity=11918087`,"http://www.iquanwai.com/images/logo.png");
     showOptionMenu();
     alert("您可以分享啦");
   }
