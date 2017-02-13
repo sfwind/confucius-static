3ec092024e2686932b3397eb21f338e71397b1f5
nethunder <17089113140@163.com>
2017-02-13 17:06:49 +0800

    fix 修改文案

--- src/modules/personal/PointTip.tsx
+++ src/modules/personal/PointTip.tsx
@@ -6,24 +6,24 @@ import {changeTitle} from "utils/helpers"
 import "./PointTip.less"
 
 
-@connect(state=>state)
-export default class PointTip extends React.Component<any,any>{
+@connect(state => state)
+export default class PointTip extends React.Component<any,any> {
   static contextTypes = {
     router: React.PropTypes.object.isRequired
   }
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      point:null,
+    this.state = {
+      point: null,
     }
   }
 
-  componentWillMount(){
+  componentWillMount() {
     changeTitle("积分规则");
   }
-  render(){
-    const {point} = this.state;
+
+  render() {
     return (
       <div className="point-tip">
         <div className="point-tip-title">
@@ -31,13 +31,13 @@ export default class PointTip extends React.Component<any,any>{
         </div>
         <div className="point-tip-container">
           <b style={{fontSize:"14px"}}>如何获得积分？ </b><br/>
-             -完善个人信息，获得30积分<br/>
-             -报名RISE课程后，每天完成2组热身训练。题目根据难度分别对应20、30、50积分，答对就获得了相应的积分；<br/>
-             -完成专题训练，获得60积分；<br/>
-             -完成选做的应用训练，根据难度获得对应的60或80积分； <br/>
-             -优秀的作业更易收到点赞，每获得一个点赞加2积分；<br/><br/>
+          -完善个人信息，获得30积分<br/>
+          -完成RISE热身训练，根据答对题目的难度不同，获得20/30/50积分<br/>
+          -完成专题训练，获得60积分<br/>
+          -完成选做的应用训练，根据难度获得对应的60或80积分<br/>
+          -专题/应用训练每获得一个点赞加2积分，优秀的作业更易收到点赞<br/><br/>
           <b style={{fontSize:"14px"}}>积分有什么用？</b> <br/>
-            -积分换购、延长开放期等功能开发中，敬请关注
+          -积分换购、延长开放期等功能开发中，敬请关注
         </div>
       </div>
     )
