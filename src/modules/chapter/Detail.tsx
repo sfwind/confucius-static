5bc46685788ba32e064902af17380884487387a7
nethunder <17089113140@163.com>
2017-02-14 15:39:50 +0800

    向上翻页按钮修改

--- src/modules/chapter/Detail.tsx
+++ src/modules/chapter/Detail.tsx
@@ -625,7 +625,7 @@ export default class Main extends React.Component<any, any> {
 										onClick={() => this.context.router.push(`/static/course/main?courseId=${this.props.location.query.courseId}`)}>关闭</Button>
 						<section className="footer-btn">
 							<div className="direct-btn-group">
-								<div className="left-button" onClick={this.prePage.bind(this)}><Icon style={{marginRight:"15px",width:"50px",height:"30px"}} type="left_arrow_new"/></div>
+								<div className="left-button" onClick={this.prePage.bind(this)}><Icon style={{marginLeft:"15px",width:"50px",height:"30px"}} type="left_arrow_new"/></div>
 							</div>
 						</section>
 					</div> : null}
