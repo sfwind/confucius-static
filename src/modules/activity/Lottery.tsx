7934b9b140cb1757f7d9c648a9c61b5fefb267ec
三十文 <xfduan@duanxianfengdeMacBook-Pro.local>
2017-05-20 15:25:59 +0800

    discount lucky card

--- src/modules/activity/Lottery.tsx
+++ src/modules/activity/Lottery.tsx
@@ -1,35 +1,64 @@
 import * as React from "react"
-import { connect } from "react-redux"
-import { pget } from "utils/request"
-import { set, startLoad, endLoad, alertMsg } from "redux/actions"
-import { ButtonArea, Button } from "react-weui"
+import {connect} from "react-redux"
+import {pget} from "utils/request"
+import {set, startLoad, endLoad, alertMsg} from "redux/actions"
+import {ButtonArea, Button} from "react-weui"
 import "./components/lucky-card.less"
+
 
 @connect(state => state)
 export default class Lottery extends React.Component<any, any> {
 
-    static contextTypes = {
-        router: React.PropTypes.object.isRequired
-    }
+  static contextTypes = {
+    router: React.PropTypes.object.isRequired
+  }
 
-    constructor() {
-        super()
-        this.state = {}
+  constructor() {
+    super()
+    this.state = {
+      discount: '',
     }
+  }
 
-    componentDidMount(){
-        const luckyCard = require("./components/lucky-card.js")
-        this.setState({luckyCard})
-        luckyCard.case({coverColor:'#CCCCCC',ratio:.6,callback:function() {
-            this.clearCover();
-        }});
-    }
+  componentDidMount() {
+    const {dispatch} = this.props
+    getDiscount().then(res => {
+      if (res.code === 200) {
+        this.setState({
+          discount: `￥${res.msg}`,
+        })
+      } else if (res.code === 201) {
+        this.setState({
+          discount: '抱歉'
+        })
+        dispatch(alertMsg("对不起，您暂未购买过任何圈外课程！"))
+      } else if (res.code === 202) {
+        this.setState({
+          discount: '抱歉'
+        })
+        dispatch(alertMsg("您已参加过此次活动！"))
+      }
+    });
+    const luckyCard = require("./components/lucky-card.js")
+    this.setState({luckyCard})
+    luckyCard.case({
+      coverColor: '#CCCCCC', ratio: .6, callback: function () {
+        this.clearCover();
+      }
+    });
+  }
 
-    render() {
-        return (
-            <div id="scratch">
-                <div id="card">￥500</div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div id="scratch">
+          <div id="card">{this.state.discount}</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+function getDiscount() {
+  return pget("/operation/discount");
 }
