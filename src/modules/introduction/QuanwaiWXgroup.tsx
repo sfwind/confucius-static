9c9fcefec3b6faacdc5a91bdc02425d9589c729e
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-05-22 21:05:42 +0800

    change wxgroup

--- src/modules/introduction/QuanwaiWXgroup.tsx
+++ src/modules/introduction/QuanwaiWXgroup.tsx
@@ -18,24 +18,27 @@ export default class QuanwaiWXgroup extends React.Component<any,any>{
   render(){
     return (
       <div className="container" style={{textAlign:'center',marginTop:20}}>
-        <div className="tip">请大家在下方二维码中自行寻找组织，舵主会在一日内拉大家入群。如联系不上舵主，可联系小Q（见最下方的二维码）</div>
-        <div>上海舵主</div>
+        <div className="tip">想找到组织？快快扫描下面你所在地舵主（负责人）的二维码哦～如果联系不上舵主，还可以联系小Q哒（ID：quanwaizhushou）
+        </div>
+        <div>上海副舵主</div>
         <div className="qrcode"><img className="qrcode-img" src="https://www.iqycamp.com/images/wxgroup/wx_sh.jpg"/></div>
         <div>北京+天津舵主</div>
-        <div className="qrcode"><img className="qrcode-img" src="https://www.iqycamp.com/images/wxgroup/wx_bj.jpg"/></div>
-        <div>广东舵主</div>
+        <div className="qrcode"><img className="qrcode-img" src="https://www.iqycamp.com/images/wxgroup/wx_bj2.png"/></div>
+        <div>深圳舵主</div>
         <div className="qrcode"><img className="qrcode-img" src="https://www.iqycamp.com/images/wxgroup/wx_sz.jpg"/></div>
-        <div>江浙联系人</div>
+        <div>广东副舵主</div>
+        <div className="qrcode"><img className="qrcode-img" src="https://www.iqycamp.com/images/wxgroup/wx_gd.jpeg"/></div>
+        <div>江苏+浙江负责人</div>
         <div className="qrcode"><img className="qrcode-img" src="https://www.iqycamp.com/images/wxgroup/wx_jz.jpg"/></div>
         <div>福建舵主</div>
         <div className="qrcode"><img className="qrcode-img" src="https://www.iqycamp.com/images/wxgroup/wx_fj.jpg"/></div>
-        <div>河南+安徽+湖北+湖南+四川舵主</div>
-        <div className="qrcode"><img className="qrcode-img" src="https://www.iqycamp.com/images/wxgroup/wx_hn.jpg"/></div>
+        <div>河南+安徽+湖北+湖南+川渝舵主</div>
+        <div className="qrcode"><img className="qrcode-img" src="https://www.iqycamp.com/images/wxgroup/wx_hn2.png"/></div>
         <div>东北+河北+山东+山西+陕西舵主</div>
         <div className="qrcode"><img className="qrcode-img" src="https://www.iqycamp.com/images/wxgroup/wx_db.jpg"/></div>
-        <div>其他地区的舵主</div>
+        <div>境外桂黔琼甘青蒙宁新舵主</div>
         <div className="qrcode"><img className="qrcode-img" src="https://www.iqycamp.com/images/wxgroup/wx_other.jpg"/></div>
-        <div> 联系不上？找小Q帮忙</div>
+        <div>联系不上？没关系，小Q来帮您～</div>
         <div className="qrcode"><img className="qrcode-img" src="https://www.iqycamp.com/images/asst.jpeg"/></div>
       </div>
     )
