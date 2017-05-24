8ef576b6309343c4215f088250a8def9aa4806d5
Ding Zhi Jun <dingzhijun@imdada.cn>
2017-05-24 18:21:46 +0800

    change wxgroup

--- src/modules/introduction/QuanwaiWXgroup.tsx
+++ src/modules/introduction/QuanwaiWXgroup.tsx
@@ -20,16 +20,16 @@ export default class QuanwaiWXgroup extends React.Component<any,any>{
       <div className="container" style={{textAlign:'center',marginTop:20}}>
         <div className="tip">想找到组织？快快扫描下面你所在地舵主（负责人）的二维码哦～如果联系不上舵主，还可以联系小Q哒（ID：quanwaizhushou）
         </div>
-        <div>上海副舵主</div>
-        <div className="qrcode"><img className="qrcode-img" src="https://www.iqycamp.com/images/wxgroup/wx_sh.jpg"/></div>
+        <div>上海舵主</div>
+        <div className="qrcode"><img className="qrcode-img" src="https://www.iqycamp.com/images/wxgroup/wx_sh2.png"/></div>
         <div>北京+天津舵主</div>
         <div className="qrcode"><img className="qrcode-img" src="https://www.iqycamp.com/images/wxgroup/wx_bj2.png"/></div>
         <div>深圳舵主</div>
         <div className="qrcode"><img className="qrcode-img" src="https://www.iqycamp.com/images/wxgroup/wx_sz.jpg"/></div>
         <div>广东副舵主</div>
         <div className="qrcode"><img className="qrcode-img" src="https://www.iqycamp.com/images/wxgroup/wx_gd.jpeg"/></div>
-        <div>江苏+浙江负责人</div>
-        <div className="qrcode"><img className="qrcode-img" src="https://www.iqycamp.com/images/wxgroup/wx_jz.jpg"/></div>
+        <div>江苏+浙江舵主</div>
+        <div className="qrcode"><img className="qrcode-img" src="https://www.iqycamp.com/images/wxgroup/wx_jz2.png"/></div>
         <div>福建舵主</div>
         <div className="qrcode"><img className="qrcode-img" src="https://www.iqycamp.com/images/wxgroup/wx_fj.jpg"/></div>
         <div>河南+安徽+湖北+湖南+川渝舵主</div>
